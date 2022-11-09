3175f78154376e8fa67c15e5702432591a6287f5
Peyman <marchosiax@gmail.com>
2022-11-09 17:48:17 +0330

    Initial commit

---
+++ src/main/java/com/marchosiax/weelink/WeelinkApplication.java
@@ -0,0 +1,13 @@
+package com.marchosiax.weelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeelinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marchosiax/weelink/WeelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marchosiax.weelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
